538d03d7e48f0225ed3f41078813365d48572806
Stelios Savva <stelios@avocarrot.com>
2015-10-08 16:29:34 +0300

    feat(validateBidResponse) started working on validation

---
+++ test/validatorTests.js
@@ -0,0 +1,12 @@
+var should = require('should');
+var Validator = require('../lib/openrtb2_3/validator');
+
+describe('The Validator should', function() {
+	
+	before(function(){
+		this.validator = new Validator();
+	});
+
+	it('should')
+
+};
