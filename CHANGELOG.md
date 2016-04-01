62ed11daf4afbf51d812c9a6d504d01855bece1e
stelios313 <stelsavva@gmail.com>
2016-04-01 11:39:58 +0300

    feat(replaceMacros): added CHANGELOG, updated docs and tests

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+#v3.0.0
+
+##Breaking changes
+
+- Bid.replaceMacros now accepts the string that contains the MACROS we want to replace as the first argument.
+Returns the new String. 
--- test/openrtb2_3/bid.spec.js
+++ test/openrtb2_3/bid.spec.js
@@ -28,21 +28,46 @@ describe("The Bid object should", function() {
 			.build();
 
 		var imptracker = "http://trackimp.com/Pixel/Impression/?bidPrice=${AUCTION_PRICE}&bidResId=${AUCTION_ID}&data=OuJifVtEKZqw3Hw7456F-etFgvhJpYOu0&type=img";
-		bid.replaceMacros({
+		bid.replaceMacros(imptracker, {
 			'${AUCTION_ID}': bidResponseId
-		}, imptracker).should.equal(util.format("http://trackimp.com/Pixel/Impression/?bidPrice=%s&bidResId=%s&data=OuJifVtEKZqw3Hw7456F-etFgvhJpYOu0&type=img", clearPrice, bidResponseId));
+		}).should.equal(util.format("http://trackimp.com/Pixel/Impression/?bidPrice=%s&bidResId=%s&data=OuJifVtEKZqw3Hw7456F-etFgvhJpYOu0&type=img", clearPrice, bidResponseId));
 		
 	});
 
 	it("throw an error if we try to replace macros without a clearPrice", function() {
-		var bid = bidBuilder
-			.nurl('http://trackwin.com/win?pid=784170&data=OuJifVtEK&price=${AUCTION_PRICE}')
-			.adm('{"native":{"assets":[{"id":0,"title":{"text":"Test Campaign"}},{"id":1,"img":{"url":"http://cdn.exampleimage.com/a/100/100/2639042","w":100,"h":100}},{"id":2,"img":{"url":"http://cdn.exampleimage.com/a/50/50/2639042","w":50,"h":50}},{"id":3,"data":{"value":"This is an amazing offer..."}},{"id":5,"data":{"value":"Install"}}],"link":{"url":"http://trackclick.com/Click?data=soDvIjYdQMm3WBjoORcGaDvJGOzgMvUap7vAw2"},"imptrackers":["http://trackimp.com/Pixel/Impression/?bidPrice=${AUCTION_PRICE}&data=OuJifVtEKZqw3Hw7456F-etFgvhJpYOu0&type=img"]}}')
-			.build();
+		var bid = bidBuilder.build();
 
 		(function(){
-			bid.replaceMacros();
+			bid.replaceMacros('http://trackwin.com/win?pid=784170&data=OuJifVtEK&price=${AUCTION_PRICE}');
 		}).should.throw('Cannot replace macros without a clear price');
 	});
 
+	it("throw an error if we try to replace macros without passing a string", function() {
+		var bid = bidBuilder.build();
+		var bidResponseId = '1234';
+
+		(function(){
+			bid.replaceMacros({
+				'${AUCTION_ID}': bidResponseId
+			});
+		}).should.throw('Invalid string argument');
+	});
+
+	it("throw an error if we try to replace macros on empty string", function() {
+		var bid = bidBuilder.build();
+		var bidResponseId = '1234';
+
+		(function(){
+			bid.replaceMacros('', {
+				'${AUCTION_ID}': bidResponseId
+			});
+		}).should.throw('Invalid string argument');
+	});
+
 });
+
+
+
+
+
+
